c3df00c3f0a7f58a4a32f21eede8ca17c939ae76
ShayanBanerjee <shayanbanerjee96@gmail.com>
2025-11-25 14:46:21 +0000

    Updated settings for production build

--- setup.py
+++ setup.py
@@ -3,8 +3,19 @@ from setuptools import find_packages, setup
 setup(
     name="RAG-Based-Conversational-Medical-Chatbot",
     version="0.1.0",
-    author="Shayan banerjee",
+    author="Shayan Banerjee",
     author_email="shayanbanerjee96@gmail.com",
     packages=find_packages(),
-    install_requires=[]
+    python_requires=">=3.10",
+    install_requires=[
+        "langchain<1.0",
+        "jupyterlab==4.0.2",
+        "flask==3.1.1",
+        "python-dotenv==1.1.0",
+        "sentence-transformers==4.1.0",
+        "pypdf==5.6.1",
+        "langchain-pinecone<1.0",
+        "langchain-openai<1.0",
+        "langchain-contextual<1.0",
+    ],
 )
--- template.sh
+++ template.sh
@@ -11,5 +11,16 @@ touch setup.py
 touch app.py
 touch research/trials.ipynb
 touch requirements.txt
-
+# Confirmation message
 echo "Directory asnd files created successfully!"
+
+# Setting up virtual environment and installing dependencies
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Building and installing the package
+pip install build
+python -m build --wheel
+pip install dist/RAG_Based_Conversational_Medical_Chatbot-0.1-py3-none-any.whl
