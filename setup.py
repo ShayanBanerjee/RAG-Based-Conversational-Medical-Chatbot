18877301c06f91d10a09d74595f0b48d620d3946
ShayanBanerjee <shayanbanerjee96@gmail.com>
2025-11-25 14:16:34 +0000

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="RAG-Based-Conversational-Medical-Chatbot",
+    version="0.1.0",
+    author="Shayan banerjee",
+    author_email="shayanbanerjee96@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
